095336124c6ddfcc011f9f88adb9e4f79602b646
Succès Djasrane Tolba <succesdjasranetolba@gmail.com>
2025-08-05 11:42:01 +0100

    my second commit

--- controllers/authController.js
+++ controllers/authController.js
@@ -9,6 +9,61 @@ const createToken = (id) =>
     expiresIn: process.env.JWT_EXPIRES_IN || '1d',
   });
 
+// Enregistre un utilisateur
+exports.register = async (req, res) => {
+  const { name, email, password } = req.body;
+
+  try {
+    const userExists = await User.findOne({ email });
+    if (userExists)
+      return res.status(400).json({ message: 'Utilisateur existe déjà' });
+
+    const hashed = await bcrypt.hash(password, 10);
+    const otp = Math.floor(100000 + Math.random() * 900000).toString();
+
+    const user = await User.create({
+      name,
+      email,
+      password: hashed,
+      otp,
+      otpPurpose: 'email_verification',
+      otpExpire: Date.now() + 10 * 60 * 1000, // 10 minutes
+    });
+
+    await sendEmail(
+      email,
+      'Confirmez votre compte',
+      `Bonjour ${name},<br><br>Votre code de confirmation est : <b>${otp}</b><br>Il expire dans 10 minutes.`
+    );
+
+    res.status(201).json({ message: 'Inscription réussie. Vérifiez votre email.' });
+  } catch (err) {
+    res.status(500).json({ message: err.message });
+  }
+};
+
+// Connexion utilisateur
+exports.login = async (req, res) => {
+  const { email, password } = req.body;
+
+  try {
+    const user = await User.findOne({ email });
+    if (!user)
+      return res.status(400).json({ message: 'Utilisateur non trouvé' });
+
+    const valid = await bcrypt.compare(password, user.password);
+    if (!valid)
+      return res.status(400).json({ message: 'Mot de passe incorrect' });
+
+    if (!user.isVerified)
+      return res.status(403).json({ message: 'Veuillez confirmer votre email' });
+
+    const token = createToken(user._id);
+    res.json({ token });
+  } catch (err) {
+    res.status(500).json({ message: err.message });
+  }
+};
 
 // Vérifie l'OTP pour confirmation email
 exports.verifyOtp = async (req, res) => {
